67223f0ea79f50b9b58dbe0d360074661b716e1a
EmilsWebbod <emil@webbod.no>
2018-12-21 17:12:43 +0100

    Added useCrud
    Create Read Update Delete hook

--- src/react/index.ts
+++ src/react/index.ts
@@ -2,3 +2,4 @@ export * from './ReachContext';
 export * from './ReachResource';
 export * from './ReachResourceContext';
 export * from './useReach';
+export * from './useCrud';
---
+++ src/react/useCrud.ts
@@ -0,0 +1,244 @@
+import { ReachError } from '../interface';
+import { FormEvent, useRef, useState } from 'react';
+import { reachApi } from '../api';
+
+interface Functions<T> {
+  onEdit?: (data: T) => void;
+  onDelete?: () => void;
+  onCreate?: (data: T) => void;
+  onSuccess?: (data?: T) => void;
+}
+
+interface Props<T> extends Functions<T> {
+  url: string;
+  id?: keyof T;
+  data?: Partial<T>;
+  edit?: Partial<Edit<T>>;
+}
+
+interface State<T extends object> {
+  body: Partial<T>;
+  edit: Edit<Partial<T>>;
+  busy: boolean;
+  error?: ReachError;
+}
+
+export type Edit<T> = {
+  [key in keyof T]: {
+    type: string;
+    label?: string;
+    edit?: boolean;
+    required?: boolean;
+  }
+};
+
+interface Object {
+  _id?: string;
+
+  [key: string]: any;
+}
+
+type Methods = 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+export function useCrud<T extends Object>(props: Props<T>) {
+  const { url, id = '_id', data = {}, edit = {}, ...functions } = props;
+
+  const init = useRef(false);
+  const [state, setState] = useState<State<T>>({
+    body: data,
+    edit: createEditBody(edit, data, Boolean(init.current)),
+    busy: false
+  });
+
+  async function read() {
+    try {
+      const dataId = data[id];
+      setState({ ...state, busy: true });
+      const body = await reachApi(`${url}/${dataId}`);
+      setState({ ...state, body, edit: resetEdit(state.edit), busy: false });
+    } catch (e) {
+      setState({ ...state, error: e, busy: false });
+    }
+  }
+
+  function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const dataId = state.body[id];
+    postData(dataId ? 'PATCH' : 'POST')();
+  }
+
+  function DELETE() {
+    if (confirm('Er du sikker?')) {
+      postData('DELETE')();
+    }
+  }
+
+  function postData(method: Methods) {
+    return async function() {
+      if (state.busy) {
+        return;
+      }
+      let dataId = state.body[id];
+
+      if (method === 'POST') {
+        dataId = undefined;
+      } else {
+        if (!id) {
+          throw new Error('Cant PUT PATCH or DELETE without id');
+        }
+      }
+
+      try {
+        const body = setBody(method, state);
+
+        if (method === 'PUT' || method === 'POST') {
+          if (body) {
+            delete body[id];
+          }
+        }
+
+        setState({ ...state, busy: true });
+
+        let reachUrl = url + (dataId ? `/${dataId}` : '');
+        const response = await reachApi<T>(reachUrl, { method, body });
+
+        setState({
+          ...state,
+          edit: resetEdit(state.edit),
+          body: { ...state.body, ...response },
+          busy: false
+        });
+
+        handleSuccess(method, functions, response);
+      } catch (e) {
+        setState({ ...state, error: e, busy: false });
+      }
+    };
+  }
+
+  function get<K extends keyof T>(key: K) {
+    return {
+      value: state.body[key],
+      ...state.edit[key]
+    };
+  }
+
+  function set<K extends keyof T>(key: K) {
+    return async function(e: T[K] | any) {
+      const value: T[K] =
+        e && 'target' in e ? e.target && (e.target.value as T[K]) : e;
+
+      if (state.body[key] === value) {
+        return;
+      }
+
+      state.body[key] = value;
+      if (!state.edit[key]) {
+        state.edit[key] = {
+          ...state.edit[key],
+          edit: true
+        };
+      } else {
+        state.edit[key].edit = true;
+      }
+
+      await setState({ ...state });
+    };
+  }
+
+  init.current = true;
+  return {
+    state,
+    get,
+    set,
+    read,
+    POST: postData('POST'),
+    PUT: postData('PUT'),
+    PATCH: postData('PATCH'),
+    DELETE,
+    onSubmit
+  };
+}
+
+function createEditBody<T extends object>(
+  edit: Partial<Edit<T>>,
+  body: T,
+  init?: boolean
+) {
+  if (init) {
+    return edit as Edit<T>;
+  }
+  const obj: Partial<Edit<T>> = edit;
+  for (const key in body) {
+    if (body.hasOwnProperty(key)) {
+      if (!obj[key] || !('edit' in obj[key])) {
+        // @ts-ignore
+        obj[key] = {
+          ...(obj[key] ? obj[key] : {}),
+          edit: false
+        };
+      }
+    }
+  }
+  return obj as Edit<T>;
+}
+
+function resetEdit<T extends object>(edit: Edit<T>) {
+  for (const key in edit) {
+    if (edit.hasOwnProperty(key)) {
+      edit[key].edit = false;
+    }
+  }
+  return edit;
+}
+
+function setBody<T extends object>(method: Methods, state: State<T>) {
+  let body;
+
+  if (method === 'PUT' || method === 'POST') {
+    body = { ...state.body };
+  } else if (method === 'PATCH') {
+    body = getEditedBody(state);
+  }
+
+  return body;
+}
+
+function getEditedBody<T extends object, K extends keyof T>(body: State<T>) {
+  const retBody: Partial<T> = {};
+  for (const key in body.body) {
+    if (
+      body.body.hasOwnProperty(key) &&
+      body.edit[key] &&
+      body.edit[key].edit
+    ) {
+      retBody[key] = body.body[key];
+    }
+  }
+  return retBody;
+}
+
+function handleSuccess<T>(method: Methods, functions: Functions<T>, res: T) {
+  const { onEdit, onCreate, onDelete, onSuccess } = functions;
+  switch (method) {
+    case 'PUT':
+    case 'PATCH':
+      if (typeof onEdit === 'function') {
+        onEdit(res);
+      }
+      break;
+    case 'POST':
+      if (typeof onCreate === 'function') {
+        onCreate(res);
+      }
+      break;
+    case 'DELETE':
+      if (typeof onDelete === 'function') {
+        onDelete();
+      }
+  }
+
+  if (typeof onSuccess === 'function') {
+    onSuccess(res);
+  }
+}
